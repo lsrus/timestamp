7abb7e84b78c6cf1eab4a99bf8e365273f2907d9
lsrus <lcthornhill@gmail.com>
2016-03-31 02:42:49 +0100

    debugging  server response

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ express = require('express');
 port =  (process.env.PORT|| 8080);
 
 var app = express()
-app.use('/:path', function(req, res){
-  res.send(path);
+app.use('/:date', function(req, res){
+  res.send(req.params.date);
 })
 app.listen(port)
 
