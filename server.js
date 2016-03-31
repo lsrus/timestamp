17d6c39761133b6d970413f5e3b1541c84ad5c63
lsrus <lcthornhill@gmail.com>
2016-03-31 02:46:36 +0100

    return dateString

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ port =  (process.env.PORT|| 8080);
 
 var app = express()
 app.use('/:date', function(req, res){
-  res.send(req.params.date);
+  res.send(new Date(req.params.date).toDateString());
 })
 app.listen(port)
 
