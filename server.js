3c2c8047159e1e82f6673ebe73de189727c6da2b
lsrus <lcthornhill@gmail.com>
2016-03-31 02:49:11 +0100

    closing bracket added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ port =  (process.env.PORT|| 8080);
 
 var app = express()
 app.use('/:date', function(req, res){
-  res.send(new Date(req.params.date.substring(1).toDateString());
+  res.send(new Date(req.params.date.substring(1).toDateString()));
 })
 app.listen(port)
 
