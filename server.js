29a2f5110d3bded067f3675c9ad86b16bc72f798
lsrus <lcthornhill@gmail.com>
2016-03-31 01:55:39 +0100

    corrected server.js error

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 express = require('express');
 port = env.process.PORT|| 8080;
 
-
-express.get('/', function(req, res){
+var app = express()
+app.get('/', function(req, res){
   res.send('Here');
 }
 
-express.listen('port')
